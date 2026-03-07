a45b436cc2f84cab1bc6d281922c621ee0b875d0
sierrasharpe <sierranoelsharpe@gmail.com>
2026-03-06 18:05:44 -0800

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -50,9 +50,9 @@ def ai_ask(prompt, data=None, temperature=0.5, max_tokens=250, model="mistral-sm
     
 # Streamed response emulator
 def response_generator():
-  response = ai_ask("Pretend you are a very friendly and helpful person.
-Please provide a response given the provided context. Please provide the
-response only with no before or after commentary.",
+  response = ai_ask("Pretend you are a very friendly and helpful person."
+  "Please provide a response given the provided context. Please provide the"
+  "response only with no before or after commentary.",
                       data=st.session_state.messages,
                       api_key=st.secrets["apikey"])
   for word in response.split():
