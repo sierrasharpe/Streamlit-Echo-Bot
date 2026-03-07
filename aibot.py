c01d0397b2501265538a97d2fe7e1ea7fde0bae2
sierrasharpe <sierranoelsharpe@gmail.com>
2026-03-06 18:03:58 -0800

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -10,8 +10,8 @@ def ai_ask(prompt, data=None, temperature=0.5, max_tokens=250, model="mistral-sm
       api_key = globals()["idToken"]
       api_url = "https://llm.boardflare.com"
   else:
-    return "Login on the Functions tab for limited demo usage, or sign up for a free Mistral AI account at https://console.mistral.ai/ and add your own
-    api_key."
+    return "Login on the Functions tab for limited demo usage, or sign up for a free Mistral AI account"
+            "at https://console.mistral.ai/ and add your own api_key."
   if not isinstance(temperature, (float, int)) or not (0 <= float(temperature) <= 2):
     return "Error: temperature must be a float between 0 and 2 (inclusive)"
   if not isinstance(max_tokens, (float, int)) or not (5 <= float(max_tokens) <= 5000):
